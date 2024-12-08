9ab44dfcf67d1ee6952922ed782b8d7ac6d9f928
Samurai_Chia <jyjzone@163.com>
2024-12-08 15:29:31 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut s1 = String::from("hello_world");
+
+    let mut s2 = change(&mut s1);
+
+    println!("---------the value of s1={s1}----------");
+
+    //println!("---------the value of s1={s2}----------");
+
+}
+
+fn change(input_data:&mut String){
+    input_data.push_str("_this_is_devil")
 }
